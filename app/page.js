ffeb171d2e69e0c5e129d39c4349d3c517d731ce
Orit Veiner <orit.veiner@gmail.com>
2023-05-28 22:42:55 +0300

    Remove for dev use default values

--- app/page.js
+++ app/page.js
@@ -225,25 +225,13 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoadingEffect, setIsLoadingEffect] = useState(true);
 
-  // const [senderName, setSenderName] = useState("");
-  // const [senderEmail, setSenderEmail] = useState("");
-  // const [companyName, setCompanyName] = useState("");
-  // const [mailContent, setMailContent] = useState("");
-  // const [responseLength, setResponseLength] = useState("");
-  // const [tone, setTone] = useState("");
-  // const [notes, setNotes] = useState("");
-  // Dev use only
-  const [senderName, setSenderName] = useState("John Smith");
-  const [senderEmail, setSenderEmail] = useState("johnsmith@example.com");
-  const [companyName, setCompanyName] = useState("PromptChainer");
-  const [mailContent, setMailContent] = useState(
-    "Hey, I was just wondering if you could help me understand the difference between a turtle and a tortoise? Also, can PromptChainer help me improve my golf swing? Cheers!"
-  );
-  const [responseLength, setResponseLength] = useState("medium");
-  const [tone, setTone] = useState("friendly");
-  const [notes, setNotes] = useState(
-    "The user is new to PromptChainer and might need a simple step-by-step explanation."
-  );
+  const [senderName, setSenderName] = useState("");
+  const [senderEmail, setSenderEmail] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [mailContent, setMailContent] = useState("");
+  const [responseLength, setResponseLength] = useState("");
+  const [tone, setTone] = useState("");
+  const [notes, setNotes] = useState("");
 
   const [randomSentence, setRandomSentence] = useState(
     loaderSentences[Math.floor(Math.random() * loaderSentences.length)]
